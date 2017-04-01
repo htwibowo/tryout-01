c01fef3317cbbd4ba1d4b29a9974648224e5a504
Harry Tri Wibowo <mail@htwibowo.co>
2017-04-01 13:23:33 +0700

    yield - using generator to yield filename into console git push origin master

--- yield/index.js
+++ yield/index.js
@@ -1,6 +1,17 @@
-'use strict'
 const fs = require('fs')
 
-function *getFileList() {
+function *fileIterator() {
+    const files = fs.readdirSync('./samples/')
+    for(let i = 0; i < files.length; i++) {
+        yield files[i]
+    }
 }
 
+const fileI = fileIterator()
+let currentFile
+
+do {
+    currentFile = fileI.next()
+
+    console.log(currentFile.value || "[EOF]")
+} while (!currentFile.done)
