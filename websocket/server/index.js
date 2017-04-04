593f3565e247b36a74c9177564ceeaaf2bbc7a10
Harry Tri Wibowo <mail@htwibowo.co>
2017-04-04 22:33:00 +0700

    websocket - update server

--- websocket/server/index.js
+++ websocket/server/index.js
@@ -4,6 +4,17 @@ var app = require('express')();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 
+const messages = [
+    "Hello There",
+    "Pesan Random #2",
+    "Pesan Random #3",
+    "Pesan Random #4",
+    "Pesan Random #5",
+    "Harry Ganteng",
+    "Pesan Random #7",
+    "Pesan Random #8",
+];
+
 server.listen(3100, '0.0.0.0');
 
 app.get('/', function (req, res) {
@@ -11,10 +22,14 @@ app.get('/', function (req, res) {
 });
 
 io.on('connection', function (socket) {
-    socket.emit('connected', { hello: 'Hello, Selamat Datang' });
+    socket.emit('connected', { msg: "Terkoneksi dengan server" });
 
-    socket.on('connected', msg => {
-        console.log('pesan dari client: ', msg)
+    socket.on('pinged', () => {
+        const idx = Math.floor(Math.random() * messages.length);
+
+        socket.emit('connected', { msg: messages[idx] });
+
+        console.log('pesan yang dikirim ke client: ', { msg: messages[idx] })
     })
 
     console.log('Ada koneksi baru');
